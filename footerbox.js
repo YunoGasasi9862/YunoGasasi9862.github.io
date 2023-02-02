c08174e4817920c1c3de5c6488867d5846b03a67
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-02-02 21:06:17 +0300

    ss

--- footerbox.js
+++ footerbox.js
@@ -4,7 +4,6 @@ $(function()
     var height= Number($("body").height());
     var newHeight=height-20;
     $(".copyright").css("top", newHeight);
-    $(".footer-box").css("top", newHeight-$(".footer-box").height());
 
    
     
