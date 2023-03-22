e76ac80115b5db04adbe30a08151e2de376d4282
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2023-03-22 21:37:06 +0300

    Changes

--- GamesDeveloped.js
+++ GamesDeveloped.js
@@ -1,6 +1,6 @@
 var count=0;
 var StringLoc="";
-var TextArray=["Kiwi Eater", "Angry Fish", "Box Runner", "Color Switch Clone", "Fighter Planes"]
+var TextArray=["Escape The Nemesis","Kiwi Eater", "Angry Fish", "Box Runner", "Color Switch Clone", "Fighter Planes"]
 var LinkArray=["https://github.com/YunoGasasi9862/2DPlatformer", "https://github.com/YunoGasasi9862/AngryBirdsSelf",
  "https://github.com/YunoGasasi9862?tab=repositories", "https://github.com/YunoGasasi9862/Color-Changer","https://github.com/YunoGasasi9862?tab=repositories"];
 
@@ -8,41 +8,80 @@ var LinkArray=["https://github.com/YunoGasasi9862/2DPlatformer", "https://github
 
   
      $("body").height(window.innerHeight);
-    
+   
+     
+if(count==0)
+{
+  document.getElementById("text"+`${5}`).style.display="none";
+
+  
+  $(".SlideShow").empty().append( `
+  
+  <button class="lt" type="button" name="LT" onclick="ChangePictureLeft()">
+                      &lt;
+                  </button>
+                  </button>
+                  <div class="first-Picture">
+                  <video class="FP" width="640" height="300" controls>
+                  <source src="P0.mp4" type="video/mp4">
+                  </video>
+                      <div class="description"><a id ="ds" href="https://github.com/YunoGasasi9862/2DPlatformer" target="_blank">Escape The Nemesis
+                      </a>
+                      </div>
+                  </div>
+                  <button class="gt" type="button" name="GT" onclick="ChangePicture()">
+                      &gt;
+                  </button>
+
+            
+  
+  
+`)
+}
+ 
+ 
      
      
 })
 function ChangePicture()
 {
- 
+
+
   var elements=document.getElementsByClassName("FP"); //this returns an array
+  console.log(count);
   //return array!!
-
-       
-
-
+  count++;
   
-         if(count<5 && count >=0) 
+
+         if(count<6 && count >0) 
           {
-            document.getElementById("ds").innerHTML=TextArray[count];
-            document.getElementById("ds").setAttribute("href", LinkArray[count]);
-            for(var i=0; i<5; i++)
+            
+            if(count>0)
             {
-              
-              
-
-               if(i==count)
-               {
-                document.getElementById("text"+`${i}`).style.display="block";
-                
-               }else
-               {
-                document.getElementById("text"+`${i}`).style.display="none";
-               }
-
-            }
+                      $(".SlideShow").empty().append(`<button class="lt" type="button" name="LT" onclick="ChangePictureLeft()">
+                      &lt;
+                  </button>
+                  <div class="first-Picture">
+                      <img class="FP" src="P5.jpg">
+                      <div class="description"><a id ="ds" href="https://github.com/YunoGasasi9862/2DPlatformer" target="_blank">Kiwi Eater
+                      </a>
+                      </div>
+                  </div>
+          
+          
+                  <button class="gt" type="button" name="GT" onclick="ChangePicture()">
+                      &gt;
+                  </button>`)
+                  document.getElementById("ds").innerHTML=TextArray[count];
+                  document.getElementById("ds").setAttribute("href", LinkArray[count]);
+          }
+          
+          
+          
+             ForLoop();
             StringLoc="P"+count+".jpg";
-            count++;
+       
+            
           }else
           {
             document.getElementById("text"+`${count-1}`).style.display="none";
@@ -51,26 +90,50 @@ function ChangePicture()
             document.getElementById("text"+`${count}`).style.display="block";
             document.getElementById("ds").innerHTML=TextArray[count];
             document.getElementById("ds").setAttribute("href", LinkArray[count]);
+            $(".SlideShow").empty().append( `
+              
+              <button class="lt" type="button" name="LT" onclick="ChangePictureLeft()">
+                                  &lt;
+                              </button>
+                              </button>
+                              <div class="first-Picture">
+                              <video class="FP" width="640" height="300" controls>
+                              <source src="P0.mp4" type="video/mp4">
+                              </video>
+                                  <div class="description"><a id ="ds" href="https://github.com/YunoGasasi9862/2DPlatformer" target="_blank">Escape The Nemesis
+                                  </a>
+                                  </div>
+                              </div>
+                              <button class="gt" type="button" name="GT" onclick="ChangePicture()">
+                                  &gt;
+                              </button>
 
-            count++;
+            
+  
+  
+`)
+            
+           
           }
 
-    elements[0].src= StringLoc;
+    if(count!=0)
+    {
+      elements[0].src= StringLoc;
 
+    }
 
+   
+ 
+    
+  
     
 }
 
-function ChangePictureLeft()
+function ForLoop()
 {
-
-  var elements1=document.getElementsByClassName("FP");
- 
-   if(count<5 && count>=0)
-   {
-    count--;
-    for(var i=0; i<5; i++)
+  for(var i=0; i<6; i++)
     {
+      
       
 
        if(i==count)
@@ -83,24 +146,99 @@ function ChangePictureLeft()
        }
 
     }
-    document.getElementById("ds").innerHTML=TextArray[count];
-    document.getElementById("ds").setAttribute("href", LinkArray[count]);
+ 
 
 
     StringLoc="P" + count + ".jpg";
+    
+}
+
+function ChangePictureLeft()
+{
+
+  var elements1=document.getElementsByClassName("FP");
+  count--;
+
+   if(count<6 && count>=0)
+   {
+    if(count==0)
+    {
+      $(".SlideShow").empty().append( `
+              
+      <button class="lt" type="button" name="LT" onclick="ChangePictureLeft()">
+                          &lt;
+                      </button>
+                      </button>
+                      <div class="first-Picture">
+                      <video class="FP" width="640" height="300" controls>
+                      <source src="P0.mp4" type="video/mp4">
+                      </video>
+                          <div class="description"><a id ="ds" href="https://github.com/YunoGasasi9862/2DPlatformer" target="_blank">Escape The Nemesis
+                          </a>
+                          </div>
+                      </div>
+                      <button class="gt" type="button" name="GT" onclick="ChangePicture()">
+                          &gt;
+                      </button>
+
+    
+
+
+`)
+    }
+    if(count>0)
+    {
+              $(".SlideShow").empty().append(`<button class="lt" type="button" name="LT" onclick="ChangePictureLeft()">
+              &lt;
+          </button>
+          <div class="first-Picture">
+              <img class="FP" src="P5.jpg">
+              <div class="description"><a id ="ds" href="https://github.com/YunoGasasi9862/2DPlatformer" target="_blank">Kiwi Eater
+              </a>
+              </div>
+          </div>
+  
+  
+          <button class="gt" type="button" name="GT" onclick="ChangePicture()">
+              &gt;
+          </button>`)
+          document.getElementById("ds").innerHTML=TextArray[count];
+          document.getElementById("ds").setAttribute("href", LinkArray[count]);
+  }
+    ForLoop();
+    document.getElementById("ds").innerHTML=TextArray[count];
+    document.getElementById("ds").setAttribute("href", LinkArray[count]);
     
    }
   if(count<0)
   {
 
-      count=4;
+      count=5;
       document.getElementById("text"+`${count}`).style.display="block";
       StringLoc="P" + count + ".jpg";
+     
       document.getElementById("ds").innerHTML=TextArray[count];
       document.getElementById("ds").setAttribute("href", LinkArray[count]);
+      $(".SlideShow").empty().append(`<button class="lt" type="button" name="LT" onclick="ChangePictureLeft()">
+      &lt;
+  </button>
+  <div class="first-Picture">
+      <img class="FP" src="P5.jpg">
+      <div class="description"><a id ="ds" href="https://github.com/YunoGasasi9862/2DPlatformer" target="_blank">Kiwi Eater
+      </a>
+      </div>
+  </div>
+
+
+  <button class="gt" type="button" name="GT" onclick="ChangePicture()">
+      &gt;
+  </button>`)
+  document.getElementById("text"+`${0}`).style.display="none";
+
 
     }
   
-    elements1[0].src= StringLoc;
+    if(count!=0 )
+       elements1[0].src= StringLoc;
 
 }
